c4f9169903079f0ca1c4c04a244590238a300313
ZacharyLikesNoodles <zachary.whitehead@westtown.edu>
2021-06-19 16:32:31 -0400

    Add other files

---
+++ madlibs.js
@@ -0,0 +1,30 @@
+function upper(el) {
+    var str = el.value; 
+    el.value = str.toUpperCase();
+}
+
+function createMessage() {
+    document.getElementById("result").style.display = "inline";
+    document.getElementById("word1").innerHTML =
+      document.getElementById("pluralNoun").value;
+    document.getElementById("word2").innerHTML =
+      document.getElementById("noun1").value;
+    document.getElementById("word3").innerHTML =
+      document.getElementById("noun2").value;
+    document.getElementById("word4").innerHTML =
+      document.getElementById("song").value;
+    var verb = document.getElementById("verb").value; 
+    document.getElementById("word5").innerHTML =
+      verb.charAt(0).toUpperCase() + verb.slice(1);
+
+    upper(document.getElementById("pluralNoun"));
+    upper(document.getElementById("noun1"));
+    upper(document.getElementById("noun2"));
+    upper(document.getElementById("song"));
+    upper(document.getElementById("verb"));
+}
+
+function goAgain() {
+    location.reload();
+    return false;
+}
